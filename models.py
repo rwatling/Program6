9d38acec18e17ebdd1cd58054e25e929033e38b5
Robert Watling <rwatling0902@gmail.com>
2020-12-11 14:38:19 -0500

    q1 done

--- models.py
+++ models.py
@@ -27,6 +27,7 @@ class PerceptronModel(object):
         Returns: a node containing a single number (the score)
         """
         "*** YOUR CODE HERE ***"
+        return nn.DotProduct(self.w, x)
 
     def get_prediction(self, x):
         """
@@ -35,12 +36,30 @@ class PerceptronModel(object):
         Returns: 1 or -1
         """
         "*** YOUR CODE HERE ***"
+        dot = nn.as_scalar(self.run(x))
+
+        if dot >= 0:
+            return 1
+        else:
+            return -1
+
 
     def train(self, dataset):
         """
         Train the perceptron until convergence.
         """
         "*** YOUR CODE HERE ***"
+
+        while True:
+            converge = True
+
+            for x, y in dataset.iterate_once(1):
+                if nn.as_scalar(y) != self.get_prediction(x):
+                    converge = False
+                    nn.Parameter.update(self.w, x, nn.as_scalar(y))
+
+            if converge:
+                break
 
 class RegressionModel(object):
     """
